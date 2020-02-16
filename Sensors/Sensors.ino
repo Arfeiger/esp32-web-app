f0d3feb2944395187d22ad582df7735680e10d20
Inigo Turrientes <inigo.turrientesvillanueva@edu.turkuamk.fi>
2020-02-16 20:02:13 +0200

    Created sensors code.

---
+++ Sensors/Sensors.ino
@@ -0,0 +1,18 @@
+/*
+ * Sensors
+ * 
+ * Sensors code.
+ * 
+ * Created 16/2/2020
+ * by Íñigo Turrientes
+ */
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
