125de043f6eb11373e47336ec2af110c9af9e300
Inigo Turrientes <inigo.turrientesvillanueva@edu.turkuamk.fi>
2020-02-20 01:01:11 +0200

    Modified temp_and_humidity_check.ino due to changes in library.

--- Arduino testing/temp_and_humidity_check/temp_and_humidity_check.ino
+++ Arduino testing/temp_and_humidity_check/temp_and_humidity_check.ino
@@ -1,44 +1,19 @@
-#include <Adafruit_Sensor.h>
-#include <DHT.h>
-#include <DHT_U.h>
-
-
-
-#define DHT_PIN 4
-#define DHT_TYPE DHT11
-
-DHT_Unified dht(DHT_PIN, DHT_TYPE);
-
-uint32_t delayMS;
-
-void setup(){
-  Serial.begin(9600); //Start serial communication with the Arduino board.
-  Serial.println("DHT11: Humidity and temperature check.\n");
+#include <dht11.h>
+dht11 sensor;
+#define DHT11_PIN 4
   
-  dht.begin(); //Initialize device.
-  sensor_t sensor;
-  delayMS = sensor.min_delay / 1000; // Set delay between sensor readings based on sensor details.
+void setup(){
+  Serial.begin(9600);
+  Serial.println("Ambient temperature and humidity sensor test");
 }
   
 void loop(){
-  delay(delayMS); //Wait between reads.
-  Serial.println("\n");
-  sensors_event_t event;
-  dht.temperature().getEvent(&event);
-  if (isnan(event.temperature)) { //Display error message in case of an error occurs reading temperature.
-    Serial.println(F("Error reading temperature!"));
-  } else {
-    Serial.print(F("Temperature: "));
-    Serial.print(event.temperature);
-    Serial.println(F("°C"));
-  }
-  // Get humidity event and print its value.
-  dht.humidity().getEvent(&event);
-  if (isnan(event.relative_humidity)) { //Display error message in case of an error occurs reading humidity.
-    Serial.println(F("Error reading humidity!"));
-  } else {
-    Serial.print(F("Humidity: "));
-    Serial.print(event.relative_humidity);
-    Serial.println(F("%"));
-  }
+  sensor.read(DHT11_PIN);    // READ DATA
+  Serial.print("\nAmbient temperature: ");
+  Serial.print(sensor.temperature);
+  Serial.println(" C");
+  Serial.print("Ambient humidity: ");
+  Serial.print(sensor.humidity);
+  Serial.println(" %");
+  delay(1000);
 }
